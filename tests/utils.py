a78dc63881cf0eb9de0309df32e298e2b0c888e3
Ryker Fish <rfish@mines.edu>
2025-04-15 20:45:29 -0600

    add tz param to tests

--- tests/utils.py
+++ tests/utils.py
@@ -9,10 +9,18 @@ wind_directions[30:60] -= 40*np.abs(np.sin(6*fake_times[30:60]))
 wind_speeds = np.array(wind_speeds)
 wind_directions = np.array(wind_directions)
 
-params = {"obs_dt":60, "sim_dt":1,"puff_dt":5,
-          "simulation_start": pd.to_datetime('2022-01-01 12:00:00'), "simulation_end": pd.to_datetime('2022-01-01 13:00:00'),
-          "emission_rates" : np.array([3]), "source_coordinates": np.array([[25, 25, 5]]),
-          "wind_speeds": wind_speeds, "wind_directions": wind_directions}
+params = {
+    "obs_dt": 60,
+    "sim_dt": 1,
+    "puff_dt": 5,
+    "simulation_start": pd.to_datetime("2022-01-01 12:00:00"),
+    "simulation_end": pd.to_datetime("2022-01-01 13:00:00"),
+    "emission_rates": np.array([3]),
+    "source_coordinates": np.array([[25, 25, 5]]),
+    "wind_speeds": wind_speeds,
+    "wind_directions": wind_directions,
+    "time_zone": "America/Denver",
+}
 
 grid_params = {"nx":51, "ny":51, "nz":11, "grid_coordinates":np.array([0, 0, 0, 50, 50, 10])}
 
